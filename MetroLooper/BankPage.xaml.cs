ca71133675d83f68b9fba676e415f04099755cc7
Colin <cmfrancis24@gmail.com>
2013-07-18 03:25:46 -0700

    comment

--- MetroLooper/BankPage.xaml.cs
+++ MetroLooper/BankPage.xaml.cs
@@ -35,7 +35,7 @@ namespace MetroLooper
             InitializeComponent();
             viewModel = MainViewModel.Instance;
             this.DataContext = viewModel;
-            if (!settings.Contains("projects"))
+            //if (!settings.Contains("projects"))
             {
                 settings["projects"] = new ObservableCollection<Project>();
                 ((ObservableCollection<Project>)settings["projects"]).Add(new Project("Project One"));
